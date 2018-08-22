1d824fa5bcd39f60ae1e19cb7252a9d2e8e3dc2b
Kathy Wang <kathy.x.wang@gmail.com>
2018-08-22 00:58:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,3 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  Object.assign({}, object);
+}
